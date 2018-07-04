064e2308e1d9a142d253e47d78c59c8df2e55df2
hieunba <hi3unb@gmail.com>
2018-07-04 15:25:49 +0700

    Explicitly define node attributes based on php version

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@ include_recipe 'php_fpm::ppa'
 include_recipe 'php_fpm::prepare'
 
 php_packages = if node['php_fpm']['version']
-  node['php_fpm']['packages'].map { |p| p.insert 3, node['php_fpm']['version'] }
+  node.default['php_fpm']['packages'].map { |p| p.insert 3, node['php_fpm']['version'] }
 else
   node['php_fpm']['packages']
 end
